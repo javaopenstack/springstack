6473521c723402325b2fa2f5fcbf423c464ba8f7
iotechno <iotechno@fe5191dd-1a20-434f-8ff2-6b24c97af38c>
2017-04-05 13:25:55 +0000

    rimossi file inutili

--- trunk/msa-parent/core/dbserviceimpl/src/test/java/org/java/openstack/service/AbstractTest.java
+++ trunk/msa-parent/core/dbserviceimpl/src/test/java/org/java/openstack/service/AbstractTest.java
@@ -1,10 +1,12 @@
 package org.java.openstack.service;
 
 import java.io.File;
-
-import javax.naming.Context;
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.PropertyConfigurator;
 import org.java.openstack.dbserviceimpl.config.CoreConfig;
 import org.java.openstack.tools.configuration.Configuration;
 import org.java.openstack.tools.configuration.Configuration.Datasources;
@@ -12,7 +14,6 @@ import org.java.openstack.tools.configuration.Configuration.Datasources.Datasour
 import org.java.openstack.tools.server.JndiInitializer;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner; 
 
@@ -20,21 +21,36 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations={"classpath*:configuration/spring/test-config.xml","classpath*:configuration/spring/*-service-context.xml"}) 
 public abstract class AbstractTest {
 	@BeforeClass
-	public static void before(){
+	public static void before() throws Exception{
+		Properties properties = new Properties();
+		properties.setProperty( "log4j.rootLogger", "INFO, stdout" );
+		properties.setProperty( "log4j.appender.stdout", "org.apache.log4j.ConsoleAppender" );
+		properties.setProperty( "log4j.appender.stdout.Target", "System.out" );
+		properties.setProperty( "log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout" );
+		properties.setProperty( "log4j.appender.stdout.layout.ConversionPattern", "%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L - %m%n" );
+		properties.setProperty( "log4j.logger.jdbc", "ERROR" );
+		properties.setProperty( "log4j.logger.jdbc.sqlonly", "INFO" );
+		properties.setProperty( "log4j.logger.jdbc.resultset", "ERROR" );
+		properties.setProperty( "log4j.logger.jdbc.connection", "ERROR" );
+		properties.setProperty( "log4j.logger.jdbc.sqltiming", "ERROR" );
+		properties.setProperty( "log4j.logger.jdbc.audit", "ERROR" );
+		PropertyConfigurator.configure( properties );
+		ClassLoader loader = AbstractTest.class.getClassLoader();  
 		
-		if ( StringUtils.isEmpty(System.getProperty(CoreConfig.APP_NAME_VARIABLE_NAME)) ){
-			File appHome = new File(AbstractTest.class.getClassLoader().getResource(".").getFile(), "test_home");
-			
-			System.setProperty(CoreConfig.APP_NAME_VARIABLE_NAME, appHome.toString());
+		if ( StringUtils.isEmpty(System.getProperty(CoreConfig.ENV_NAME)) ){
+			File appHome = new File(loader.getResource("configuration").getFile(), "env_home");
+			System.setProperty(CoreConfig.ENV_NAME, appHome.toString());
 		}
+		 
 		Configuration configuration = new Configuration();
 		configuration.setDatasources(new Datasources());
 		Datasource datasource = new Datasource();
-		datasource.setClassname("org.h2.Driver");
 		datasource.setJndi("java:comp/env/jdbc/data");
+		datasource.setUser("sa");
 		datasource.setPassword("");
 		datasource.setUrl("jdbc:h2:mem:test;MODE=MySQL");
-		datasource.setUser("sa");
+		datasource.setClassname("org.h2.Driver");
+		datasource.setValidationQuery("SELECT 1");
 		configuration.getDatasources().getDatasource().add(datasource);
 		JndiInitializer.init(configuration);
 	}
