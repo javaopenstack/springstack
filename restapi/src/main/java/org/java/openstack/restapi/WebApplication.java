c9d5cf4440007bbca14a8bdfe8d6206efb635c2c
iotechno <iotechno@fe5191dd-1a20-434f-8ff2-6b24c97af38c>
2017-05-05 07:46:14 +0000

    fix package

--- restapi/src/main/java/org/java/openstack/restapi/WebApplication.java
+++ restapi/src/main/java/org/java/openstack/restapi/WebApplication.java
@@ -4,8 +4,11 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.java.openstack.restapi.config.RestConfig;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 
 public class WebApplication extends AbstractAnnotationConfigDispatcherServletInitializer {
 	
@@ -33,7 +36,10 @@ public class WebApplication extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		if ( SECURITY ){
-			servletContext.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(null, false, "/*");	
+			String filterName = AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME;
+			DelegatingFilterProxy springSecurityFilterChain = new DelegatingFilterProxy();
+			registerServletFilter(servletContext, springSecurityFilterChain);
+			 
 		}
 	 
 		super.onStartup(servletContext);
